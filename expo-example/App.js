8842e099f092174dee1a6a9053fd3bf1d49e066b
Ian Rumac <ian.rumac@outlook.com>
2025-03-10 11:28:24 +0100

    Update expo example app to 2.0

--- expo-example/App.js
+++ expo-example/App.js
@@ -1,4 +1,8 @@
-import Superwall, { LogLevel, LogScope, SuperwallOptions } from '@superwall/react-native-superwall';
+import Superwall, {
+  LogLevel,
+  LogScope,
+  SuperwallOptions,
+} from '@superwall/react-native-superwall';
 import { StatusBar } from 'expo-status-bar';
 import { Button, Platform, StyleSheet, Text, View } from 'react-native';
 
@@ -8,14 +12,19 @@ export default function App() {
   options.logging.scopes = [LogScope.All];
 
   const apiKey =
-  Platform.OS === 'ios'
-    ? 'pk_5f6d9ae96b889bc2c36ca0f2368de2c4c3d5f6119aacd3d2'
-    : 'pk_d1f0959f70c761b1d55bb774a03e22b2b6ed290ce6561f85';
+    Platform.OS === 'ios'
+      ? 'pk_5f6d9ae96b889bc2c36ca0f2368de2c4c3d5f6119aacd3d2'
+      : 'pk_d1f0959f70c761b1d55bb774a03e22b2b6ed290ce6561f85';
 
-  Superwall.configure(apiKey, options, null);
+  Superwall.configure({ apiKey, options });
 
   const register = () => {
-    Superwall.shared.register('campaign_trigger');
+    Superwall.shared.register({
+      placement: 'campaign_trigger',
+      feature: () => {
+        console.log('feature triggered');
+      },
+    });
   };
 
   const identify = () => {
